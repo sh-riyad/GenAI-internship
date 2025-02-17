f99b3137040ab2b5ab06cb9c5b09efb3dd5c7de4
Sarawar Hossain <sarawarhossain13@gmail.com>
2025-02-17 20:54:42 +0600

    Add files via upload
    random update

--- chat-bot/with-api/app.py
+++ chat-bot/with-api/app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from fastapi import FastAPI
 from langchain_core.prompts import ChatPromptTemplate
 
@@ -10,12 +11,27 @@ import uvicorn
 import os
 from langchain_ollama import OllamaLLM
 from dotenv import load_dotenv
+=======
+import os
+import uvicorn
+from fastapi import FastAPI
+from langserve import add_routes
+from langchain_groq import ChatGroq
+from langchain_ollama import OllamaLLM
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+
+>>>>>>> b6d9743 (Reconnect local directory and commit changes)
 
 load_dotenv()
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+<<<<<<< HEAD
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+=======
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+>>>>>>> b6d9743 (Reconnect local directory and commit changes)
 
 app = FastAPI(
     title="Langchain Server",
@@ -25,10 +41,17 @@ app = FastAPI(
 
 add_routes(
     app,
+<<<<<<< HEAD
     ChatOpenAI(),
     path="/openai"
 )
 model = ChatOpenAI()
+=======
+    ChatGroq(),
+    path="/groq"
+)
+model = ChatGroq(model="llama-3.3-70b-versatile")
+>>>>>>> b6d9743 (Reconnect local directory and commit changes)
 
 # ollama Model (llama)
 llm = OllamaLLM(model='llama3.2:1b')
--- chat-bot/with-api/client.py
+++ chat-bot/with-api/client.py
@@ -1,7 +1,11 @@
 import requests
 import streamlit as st
 
+<<<<<<< HEAD
 def get_openai_response(input_text):
+=======
+def get_groq_response(input_text):
+>>>>>>> b6d9743 (Reconnect local directory and commit changes)
     response = requests.post("http://localhost:8000/essay/invoke",
                              json = {'input':{'topic':input_text}})
 
@@ -18,7 +22,11 @@ input_text1 = st.text_input("Write and essay on")
 input_text2 = st.text_input("Write a poem on")
 
 if input_text1:
+<<<<<<< HEAD
     st.write(get_openai_response(input_text1))
+=======
+    st.write(get_groq_response(input_text1))
+>>>>>>> b6d9743 (Reconnect local directory and commit changes)
 
 if input_text2:
     st.write(get_ollama_response(input_text2))

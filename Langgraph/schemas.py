0ac9e3a1e10613b1aa8c3b7c6f4c4762dc1f23cf
Sarawar Hossain <sarawarhossain13@gmail.com>
2025-03-11 12:32:02 +0600

    Add files via upload

---
+++ Langgraph/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+import datetime
+
+
+class StoreProductInput(BaseModel):
+    name: str
+    product_code: str
+    price: float
+    available_quantity: int
+    expiry_date: datetime.date
+    brand: str
+    product_type: str
+
+    class Config:
+        from_attributes = True
+
+class StoreServiceInput(BaseModel):
+    name: str
+    service_code: str
+    price: float
+
+    class Config:
+        from_attributes = True
